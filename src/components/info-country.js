e0b62ac3c4afbd9ee21b816f16d05c6e6b66982d
GuillaumeDuc <ducoeurguillaume@gmail.com>
2020-06-19 23:27:32 +0200

    [slider] added a slider with ranges

--- src/components/info-country.js
+++ src/components/info-country.js
@@ -4,8 +4,24 @@ import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import TrendingDownIcon from "@material-ui/icons/TrendingDown";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import Slider from "@material-ui/core/Slider";
 
 class InfoCountry extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: [1945, 2018],
+    };
+  }
+
+  valuetext(value) {
+    return `${value}°C`;
+  }
+
+  handleChange = (event, newValue) => {
+    this.setState({ value: newValue });
+  };
+
   render() {
     return (
       <Card className="info-wrapper info-world">
@@ -19,6 +35,17 @@ class InfoCountry extends Component {
           </div>
           <div className="info-row">
             <PublicIcon /> Villes avec le plus de films étrangers
+          </div>
+          <div className="info-slider">
+            <Slider
+              value={this.state.value}
+              onChange={this.handleChange}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              getAriaValueText={this.valuetext}
+              min={1945}
+              max={2018}
+            />
           </div>
         </CardContent>
       </Card>
