00b38a0264432356690c7434173f98ce8ad542a5
GuillaumeDuc <ducoeurguillaume@gmail.com>
2020-06-19 23:04:34 +0200

    Updates

---
+++ precache-manifest.a804816cecf990dec77644ac641d921d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c511bc5da4534b0d26b2a6bc4bb62984",
+    "url": "/film-attractive-front/index.html"
+  },
+  {
+    "revision": "037e79ad535e9848cf8d",
+    "url": "/film-attractive-front/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "583c718cd0ee02fcceec",
+    "url": "/film-attractive-front/static/css/main.b2a0909d.chunk.css"
+  },
+  {
+    "revision": "037e79ad535e9848cf8d",
+    "url": "/film-attractive-front/static/js/2.dc3490ab.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/film-attractive-front/static/js/2.dc3490ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583c718cd0ee02fcceec",
+    "url": "/film-attractive-front/static/js/main.63249328.chunk.js"
+  },
+  {
+    "revision": "41f9ea8ad799e9c78e31",
+    "url": "/film-attractive-front/static/js/runtime-main.81fcb1fc.js"
+  }
+]);
